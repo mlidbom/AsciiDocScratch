1e42ab3894a39eb593e658b00942df6068952288
mlidbo <magnus.lidbom@gmail.com>
2016-10-26 12:49:46 +0200

    Get to the point of having a toolkit that more or less works. Yay.

---
+++ Gulpfile.js
@@ -0,0 +1,24 @@
+var browserSync = require('browser-sync');
+var gulp = require('gulp')
+var run = require('gulp-run')
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "_book"
+        }
+    });
+});
+
+gulp.task('build-doc', function () {
+    run('npm run docs:build').exec('', function() {
+        run('echo docs:build DONE!').exec().pipe(browserSync.reload({stream:true}));
+    });
+
+})
+
+gulp.task('watch', function() {
+    gulp.watch(['**/*.adoc','!_book/**'], ['build-doc']);
+})
+
+gulp.task('default', ['build-doc', 'browser-sync', 'watch']);
